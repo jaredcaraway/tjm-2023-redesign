c41ea9dc0f3ed408d19943626cb5aa46cd772c2a
Jared Caraway <jared.caraway@gmail.com>
2023-10-31 22:34:41 -0500

    Add front page template stub

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying the front page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
+ *
+ * @package TJM_2023_Redesign
+ */
+
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+    <h1>THIS IS THE FRONT PAGE</h1>
+</main><!-- #main -->
+
+<?php get_footer(); ?>
