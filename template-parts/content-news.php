fdd0cda1cc3b35825bd461e05a8f0abe957f5ae5
Jared Caraway <jared.caraway@gmail.com>
2023-11-03 14:56:31 -0500

    Add post link button, style news archives button

---
+++ template-parts/content-news.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Template part for displaying news items.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package TJM_2023_Redesign
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'news' ); ?>>
+    <div class="container">
+        <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
+            <div class="entry-meta">
+               Entry meta
+            </div><!-- .entry-meta -->
+        </header><!-- .entry-header -->
+
+        <div class="entry-content">
+            <?php
+            the_content();
+
+            wp_link_pages( array(
+                'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tjm-2023-redesign' ),
+                'after'  => '</div>',
+            ) );
+            ?>
+        </div><!-- .entry-content -->
+
+        <footer class="entry-footer">
+        </footer><!-- .entry-footer -->
+        </div>
+    </article><!-- #post-<?php the_ID(); ?> -->
