e410594b77f492a2c24bd0d7834d5b65b2cc1aca
Jared Caraway <jared.caraway@gmail.com>
2023-11-06 23:00:24 -0600

    Add date to news post template

--- template-parts/content-news.php
+++ template-parts/content-news.php
@@ -9,12 +9,26 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'news' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('news'); ?>>
     <div class="container">
         <header class="entry-header">
-            <?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
-            <div class="entry-meta">
-               Entry meta
+            <?php the_title('<h1 class="entry-title text-center">', '</h1>'); ?>
+            <div class="entry-meta"><!-- .entry-meta -->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon">
+                        <span class="posted-on">
+                            <?php
+                            printf(
+                                esc_html__('%s', 'tjm-2023-redesign'),
+                                get_the_time(esc_html__('F j, Y', 'tjm-2023-redesign'))
+                            );
+                            ?>
+                        </span>
+                    </div>
+                    
+                    <div class="divider-custom-line"></div>
+                </div>
             </div><!-- .entry-meta -->
         </header><!-- .entry-header -->
 
@@ -26,5 +40,5 @@
 
         <footer class="entry-footer">
         </footer><!-- .entry-footer -->
-        </div>
-    </article><!-- #post-<?php the_ID(); ?> -->
+    </div>
+</article><!-- #post-<?php the_ID(); ?> -->
