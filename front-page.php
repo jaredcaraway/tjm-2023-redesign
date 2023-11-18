c01b3d925d512308ab4548523df014a503a46389
Jared Caraway <jared.caraway@gmail.com>
2023-11-17 18:16:13 -0600

    Remove hard-coded scripts from front page template

--- front-page.php
+++ front-page.php
@@ -930,12 +930,6 @@ get_header(); ?>
       </div>
     </div>
   </div>
-
-  <!-- Bootstrap core JS-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-  <!-- Core theme JS-->
-  <script src="js/scripts.js"></script>
-
 </main><!-- #main -->
 
 <?php get_footer();
